ca0a76c39315f069eaea7df00b7208caffab4c1d
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2023-05-03 17:46:12 +0530

    javafiles

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int sum=0; int a=1;  int b=1;
+        System.out.println("Enter number:");
+        int n=sc.nextInt();
+        System.out.println("Fibonacci Series:");
+        System.out.printf("%d,%d",sum,a);
+        for(int i=3;i<=n;i++){
+            sum=sum+a;
+            a=b;
+            b=sum;
+            System.out.printf(",%d",sum);
+        }
+    }
+}
