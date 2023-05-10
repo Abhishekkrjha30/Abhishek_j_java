4ae45be6204bd69fb1ad98d94e5d5896625f3b61
Abhishekkrjha30 <abhishekkrjha30@gmail.com>
2023-05-10 19:35:30 +0530

    javabasics

--- operators/Uniary.java
+++ operators/Uniary.java
@@ -2,17 +2,19 @@ package operators;
 
 public class Uniary {
     public static void main(String args[]){
-        int a=3;
-        float b=1.2f;
-        System.out.println(a++);
-        System.out.println(++a);
-        System.out.println(a);
-        System.out.println(++b);
-        System.out.println(b);
-        System.out.println(a++ - ++a);
-        System.out.println(--a - b++);
-        System.out.println(b+" " + a);
-
+//        int a=3;
+//        float b=1.2f;
+//        System.out.println(a++);
+//        System.out.println(++a);
+//        System.out.println(a);
+//        System.out.println(++b);
+//        System.out.println(b);
+//        System.out.println(a++ - ++a);
+//        System.out.println(--a - b++);
+//        System.out.println(b+" " + a);
+        byte b=12;
+        int b1=b+110;
+        System.out.println(b1);
 
     }
 }
